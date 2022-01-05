45dfb0c322072fb6e39dc42d735f7d3f9c6406f1
Running water <alsgks1218@naver.com>
2022-01-05 23:52:21 +0900

    add ITodo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
+interface ITodo {
+  id: number;
+  title: string;
+  desc: string;
+  isCompleted: boolean;
+}
+
+function App(): JSX.Element {
+  const [todos, setTodos] = useState<ITodo[]>([
+    {
+      id: 1,
+      title: "Make todo",
+      desc: "make todo with me",
+      isCompleted: false,
+    },
+  ]);
   return (
     <div>
       <h1>runningwater</h1>
